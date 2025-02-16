5b8a57e2bd452bd73043c74921c9f5a6cf233de9
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-02-16 14:28:57 -0300

    ass

--- apks.php
+++ apks.php
@@ -7,45 +7,45 @@ include("includes/head.php");
         <?php
         include("includes/navbar.php");
         ?>
+
         <!-- Projects Section-->
         <section class="py-5">
-            <div class="container px-5 mb-5">
-                <div class="text-center mb-5">
-                    <h1 class="display-5 fw-bolder mb-0"><span class="text-gradiente d-inline">APKs</span></h1>
-                </div>
-                <!-- ========== Start Section ==========
-                    Colocar aqui implementação para ler links de apk do banco de dados
-                    ========== End Section ========== -->
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-11 col-xl-9 col-xxl-8">
-                        <!-- Project Card 1-->
-                        <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
-                            <div class="card-body p-0">
-                                <div class="d-flex align-items-center">
-                                    <div class="p-5">
-                                        <h2 class="fw-bolder">BitLife BR MOD</h2>
-                                        <p>Bit life brasil com mod</p>
-                                    </div>
-                                    <a href="https://drive.google.com/file/d/1DwTBNdzuea1SzGB5a1wfAF4Cl2MMMT89/view?usp=drive_link"
-                                        target="_blank">
-                                        <img class="img-fluid" src="assets/apksImg/bitlife.png" alt="..." />
-                                    </a>
+            <div class="row justify-content-center">
+                <div class="col-md-9 col-lg-8 mx-auto"> <!-- Centralização e limite de largura -->
+                    <div class="card card-geral-black mb-3 border-0">
+                        <div class="row g-0 align-items-center">
+                            <div class="col-7 d-flex flex-column align-items-center justify-content-center text-center">
+                                <div class="card-body">
+                                    <h2 class="fw-bolder">Score! Hero MOD</h2>
+                                    <p>Score! Hero com MOD</p>
                                 </div>
                             </div>
+                            <div class="col-5">
+                                <a href="https://drive.google.com/file/d/1qnekSPIfYBMt8YjtKC224XQQkWOQ8WKp/view?usp=drive_link"
+                                    target="_blank">
+                                    <img class="img-fluid" src="assets/apksImg/scorehero.png" alt="..." />
+                                </a>
+                            </div>
                         </div>
-                        <!-- Project Card 2-->
-                        <div class="card overflow-hidden shadow rounded-4 border-0">
-                            <div class="card-body p-0">
-                                <div class="d-flex align-items-center">
-                                    <div class="p-5">
-                                        <h2 class="fw-bolder">Score! Hero MOD</h2>
-                                        <p>Score! Hero com MOD</p>
-                                    </div>
-                                    <a href="https://drive.google.com/file/d/1qnekSPIfYBMt8YjtKC224XQQkWOQ8WKp/view?usp=drive_link"
-                                        target="_blank">
-                                        <img class="img-fluid" src="assets/apksImg/scorehero.png" alt="..." />
-                                    </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center mt-4">
+                <div class="col-md-9 col-lg-8 mx-auto"> <!-- Mesmo ajuste para o segundo card -->
+                    <div class="card card-geral-black mb-3 border-0">
+                        <div class="row g-0 align-items-center">
+                            <div class="col-7 d-flex flex-column align-items-center justify-content-center text-center">
+                                <div class="card-body">
+                                    <h2 class="fw-bolder">BitLife BR MOD</h2>
+                                    <p>Bit life brasil com mod</p>
                                 </div>
+                            </div>
+                            <div class="col-5">
+                                <a href="https://drive.google.com/file/d/1DwTBNdzuea1SzGB5a1wfAF4Cl2MMMT89/view?usp=drive_link"
+                                    target="_blank">
+                                    <img class="img-fluid" src="assets/apksImg/bitlife.png" alt="..." />
+                                </a>
                             </div>
                         </div>
                     </div>
